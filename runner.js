5f5a68dc1500386b0f4f6b2fba4534f9ddc01b5d
zack.shen <zackshen0526@gmail.com>
2013-09-24 16:00:07 +0800

    lookup test suite

--- runner.js
+++ runner.js
@@ -1,3 +1,32 @@
+var fs = require('fs')
+  , path = require('path')
+  , basename = path.basename
+  , exists = fs.existsSync || path.existsSync
+  , join = path.join;
+
+function lookupFiles(path, recursive) {
+  var files = [];
+
+  if (!exists(path)) path += '.js';
+  var stat = fs.statSync(path);
+  if (stat.isFile()) return path;
+
+  fs.readdirSync(path).forEach(function(file){
+    file = join(path, file);
+    var stat = fs.statSync(file);
+    if (stat.isDirectory()) {
+      if (recursive) files = files.concat(lookupFiles(file, recursive));
+      return
+    }
+    if (!stat.isFile()
+        || basename(file).indexOf('test_') == -1
+        || basename(file)[0] == '.') return;
+    files.push(file);
+  });
+
+  return files;
+}
+
 global.requirejs = require('requirejs');
 var Mocha = require("mocha");
 
@@ -11,6 +40,6 @@ global.assert = require('assert');
 global.window = global;
 
 // add test suites
-mocha.addFile('test/utils');
+mocha.files = lookupFiles('./test', true);
 
 mocha.run();
